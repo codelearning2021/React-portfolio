b877d181e2ee9f7699b751410d5c1dcea0d0b81d
Nate J <nathan.evan.johnson@gmail.com>
2023-05-11 15:07:44 -0700

    pushing recent changes

--- src/pages/About/About.js
+++ src/pages/About/About.js
@@ -5,11 +5,13 @@ export default function About() {
     <div>
       <h1>About Page</h1>
       <p>
-        36 Year old with a decade in IT and a certificate in Full Stack Web Development.
+        36 Year old with a decade in IT and a certificate in Full Stack Web Development. Experience in training, project management, support, writing and comms, and working in large FAANG organizations.
+        
+        <br /><br />Love for music and something of a film nerd. 
       </p>
 
       <p>
-        <img src="https://github.com/codelearning2021/React-portfolio/blob/master/public/selfpic.jpg?raw=true" />
+        <img class="selfpic" src="https://github.com/codelearning2021/React-portfolio/blob/51bed748f29c80d7333f7b58ed731c4dddbdff84/public/selfpic.jpg?raw=true" />
       </p>
     </div>
   );
